4d447364a2937db05955ee239817f54107608561
Florentin Dumitrache <dumitracheflorentincristian@gmail.com>
2025-01-04 21:07:21 +0200

    feat: setup shadcn/ui

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Button } from '../components/ui/button'
 
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <Button>Click me</Button>
     </div>
   )
 }
