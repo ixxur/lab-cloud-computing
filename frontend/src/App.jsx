ab579d00c94d4b1d1fc7f13dbff36d988c1f9d95
Florentin Dumitrache <dumitracheflorentincristian@gmail.com>
2025-01-05 18:15:29 +0200

    feat: init books/:id and added not found page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
+import NotFound from './pages/NotFound'
 import SignupPage from './pages/SignUp'
 import LoginPage from './pages/Login'
 import Profile from './pages/Profile'
 import Borrows from './pages/Borrows'
 import Books from './pages/Books'
 import Home from './pages/Home'
+import Book from './pages/Book'
 
 import { ToastContainer } from 'react-toastify'
 
@@ -20,6 +22,8 @@ const App = () => {
           <Route path="/profile" element={<Profile />} />
           <Route path="/books" element={<Books />} />
           <Route path="/borrows" element={<Borrows />} />
+          <Route path="/books/:id" element={<Book />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
 
