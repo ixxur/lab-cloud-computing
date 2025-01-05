be5e2f2136037e1dfae8d56d9b1fd7995bc19445
Florentin Dumitrache <dumitracheflorentincristian@gmail.com>
2025-01-05 13:10:01 +0200

    feat: add register page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 import Home from './pages/Home'
 import LoginPage from './pages/Login'
 import SignupPage from './pages/SignUp'
 
+import { ToastContainer } from 'react-toastify'
+
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+        </Routes>
+      </Router>
+
+      <ToastContainer />
+    </>
   )
 }
 
